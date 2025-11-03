8698f76b7956e83c8ae1146bd76128619286a4fd
julio <julesrufino@gmail.com>
2025-11-03 16:03:44 -0600

    agregando menu y validacion de opciones

--- Hospital.c
+++ Hospital.c
@@ -1,6 +1,42 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdbool.h>
+
+bool validarOpcion(int *);
+bool validarEdad(int);
+bool validarGenero(char);
+bool validarNumeroDeConsultorio(int);
 
 main()
 {
+	int opcion;
+	
+    do{
     printf("Menu.\n");
+    printf("1. Alta del paciente.\n");
+    printf("2. Busqueda o edicion de paciente.\n");
+    printf("3. Listado de pacientes.\n");
+    printf("4. Baja de paciente.\n");
+    printf("5. Salir.\n");
+    
+    printf("Ingrese la opcion seleccionada: ");
+    scanf("%d", &opcion);
+    
+    while(validarOpcion(&opcion))
+    {
+    	printf("Error, ingrese debe ingresar una opcion entre 1-5.\n");
+        printf("Ingrese la opcion seleccionada: ");
+    	scanf("%d", &opcion);
+	}
+	
+	}while(opcion != 5);
+}
+
+bool validarOpcion(int *opcionPtr)
+{
+	bool validacion = false;
+	
+	if(*opcionPtr <= 0 || *opcionPtr > 5)
+		validacion = true;
+	
+	return validacion;
 }
